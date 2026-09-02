a1b7ec6a815f18e5f3d74d805c10c32c1314a5e4
peterelauda <peterelauda>
2026-09-02 17:08:47 +0800

    fix: Change font size and style in hero slide show

--- resources/js/components/HeroSlideshow.vue
+++ resources/js/components/HeroSlideshow.vue
@@ -25,7 +25,7 @@
                 </svg>
 
                 <h1
-                    class="col-lg-6 mx-auto mb-4 dynalight-regular text-white text-5xl md:text-7xl font-normal solid-shadow-text">
+                    class="col-lg-6 mx-auto mb-4 imperial-script-regular text-white text-4xl md:text-6xl font-normal solid-shadow-text">
                     "Let’s make your memories remains forever."
                 </h1>
             </div>
@@ -117,11 +117,11 @@ onUnmounted(() => {
 }
 
 .solid-shadow-text {
-    text-shadow: 1px 1px 0px rgba(0, 0, 0, 0.5),
-                 2px 2px 0px rgba(0, 0, 0, 0.3);
+    text-shadow: 1px 1px 0px rgba(0, 0, 0, 0.3),
+        1px 1px 0px rgba(0, 0, 0, 0.3);
 }
 
 .solid-shadow-icon {
-    filter: drop-shadow(1px 1px 0px rgba(0, 0, 0, 0.5));
+    filter: drop-shadow(1px 1px 0px rgba(0, 0, 0, 0.3));
 }
 </style>
